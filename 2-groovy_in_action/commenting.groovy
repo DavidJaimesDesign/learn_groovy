2c4c87e8e2fc3d61bb3fa73b543b25c2035e7abd
David Jaimes <davidjaimesdesign@gmail.com>
2017-03-09 08:52:41 -0800

    Started second chapter

---
+++ 2-groovy_in_action/commenting.groovy
@@ -0,0 +1,9 @@
+#! Shebang comment only allowedd on the first line allows for unix shells to locate the groovy bootrap script and runt code with it 
+
+//some line comment
+
+/*
+    multi 
+    line 
+    comment
+*/
