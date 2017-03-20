96465c09419d473919e734bc636e53807343653f
David Jaimes <davidjaimesdesign@gmail.com>
2017-03-20 07:47:42 -0700

    Finished formating for 0.75 level 3 not the best but will do for now

--- projects/m-math/level-3.groovy
+++ projects/m-math/level-3.groovy
@@ -3,25 +3,62 @@ class Level3Loop {
 	private Integer level
 	private Boolean lessonLoop
 	private List 	wrongAnswers
-	//private Float progress
-	Level3Loop(){
+	private Float progress
+	Level3Loop(i_progress){
 		level = 3
 		lessonLoop = true
 		wrongAnswers = []
+		progress = i_progress
 	}
 	def start(){
 		while (lessonLoop) {
-			def a = Math.abs(new Random().nextInt() % 10) + 1
-			def b = Math.abs(new Random().nextInt() % 10) + 1
-
-			def answer = System.console().readLine a + ' 3 ' + b +' = '
-			if(answer.toInteger() != a + b){
-				wrongAnswers << [a, b, answer.toInteger]
+			switch(progress) {
+				case 0.00: progress0(); break
+				case 0.25: progress25(); break
+				case 0.50: progress50(); break
+				case 0.75: progress75(); break
+				default:   progress0()
 			}
 		}
 	}
 
 	def stop(){
 		lessonLoop = false
+	}
+
+	def progress0(a = 1, b = 1){
+		def answer = System.console().readLine a + ' x ' + b +' = '
+		if(b == 13){
+			progress0(a += 1, 0)
+		} else {
+			progress0(a, b+=1)
+		}
+	}
+
+	def progress25(){
+		def a = Math.abs(new Random().nextInt() % 13) + 1
+		def b = Math.abs(new Random().nextInt() % 13) + 1
+
+		def answer = System.console().readLine a + ' x ' + b +' = '
+	}
+
+	def progress50(){
+		def a = Math.abs(new Random().nextInt() % 1000) + 10
+		def b = Math.abs(new Random().nextInt() % 10) + 1
+
+		println '   ' + a
+		println ' x ' + b
+		println '--------'
+		def answer = System.console().readLine ''
+	}
+
+	def progress75(){
+		def a = Math.abs(new Random().nextInt() % 501) + 100
+		def b = Math.abs(new Random().nextInt() % a) + 50
+
+		
+		println  a + '| ' + b
+		println '--------'
+		def answer = System.console().readLine ''	
 	}
 }
