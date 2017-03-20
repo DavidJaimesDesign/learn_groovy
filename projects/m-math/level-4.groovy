1341aca679a7bf7ab784575bcaa47f6cfdbc589a
David Jaimes <davidjaimesdesign@gmail.com>
2017-03-20 08:10:15 -0700

    Level 4 progression methods added still need to be honed in to work properly

--- projects/m-math/level-4.groovy
+++ projects/m-math/level-4.groovy
@@ -3,20 +3,21 @@ class Level4Loop {
 	private Integer level
 	private Boolean lessonLoop
 	private List 	wrongAnswers
-	//private Float progress
-	Level4Loop(){
+	private Float progress
+	Level4Loop(i_progress){
 		level = 4
 		lessonLoop = true
 		wrongAnswers = []
+		progress = i_progress
 	}
 	def start(){
 		while (lessonLoop) {
-			def a = Math.abs(new Random().nextInt() % 10) + 1
-			def b = Math.abs(new Random().nextInt() % 10) + 1
-
-			def answer = System.console().readLine a + ' 4 ' + b +' = '
-			if(answer.toInteger() != a + b){
-				wrongAnswers << [a, b, answer.toInteger]
+			switch(progress) {
+				case 0.00: progress0(); break
+				case 0.25: progress25(); break
+				case 0.50: progress50(); break
+				case 0.75: progress75(); break
+				default:   progress0()
 			}
 		}
 	}
@@ -24,4 +25,45 @@ class Level4Loop {
 	def stop(){
 		lessonLoop = false
 	}
+	//2 digit multiplication
+	def progress0(){
+		def a = Math.abs(new Random().nextInt() % 50) + 1
+		def b = Math.abs(new Random().nextInt() % 50) + 1
+
+		println '   ' + a
+		println ' x ' + b
+		println '--------'
+		def answer = System.console().readLine ''
+	}
+	//2 digit multiplication
+	def progress25(){
+		def a = Math.abs(new Random().nextInt() % 100) + 1
+		def b = Math.abs(new Random().nextInt() % 100) + 1
+
+		println '   ' + a
+		println ' x ' + b
+		println '--------'
+		def answer = System.console().readLine ''
+	}
+	//long division
+	def progress50(){
+		def a = Math.abs(new Random().nextInt() % 501) + 100
+		def b = Math.abs(new Random().nextInt() % a) + 50
+
+		
+		println  a + '| ' + b
+		println '--------'
+		def answer = System.console().readLine ''	
+	}
+	//reducing fractions
+	def progress75(){
+		def cd = Math.abs(new Random().nextInt() % 15) + 1
+		def a  = Math.abs(new Random().nextInt() % 12) * cd 
+		def b  = Math.abs(new Random().nextInt() % 12) * cd
+
+		println  a
+		println '_'
+		println b
+		def answer = System.console().readLine '='
+	}		
 }
