d8fa40a1f32057de758e7b763e6de46326efbb5c
David Jaimes <davidjaimesdesign@gmail.com>
2017-03-21 05:53:41 -0700

    Error catching added to level 1

--- projects/m-math/level-1.groovy
+++ projects/m-math/level-1.groovy
@@ -25,12 +25,31 @@ class Level1Loop {
 	def stop(){
 		lessonLoop = false
 	}
+	def checksum(answer, value_a, value_b){
+		if(answer == value_a + value_b){
+			return true
+		} else {
+			return false
+		}
+	}
 
+	def checkdiff(answer, value_a, value_b){
+		if(answer == value_a - value_b){
+			return true
+		} else {
+			return false
+		}
+
+	}
 	def progress0(){
 		def a = Math.abs(new Random().nextInt() % 8) + 1
 		def b = Math.abs(new Random().nextInt() % 8) + 1
 
 		def answer = System.console().readLine a + ' + ' + b +' = '
+		
+		if(!checksum(answer.toInteger(), a, b)){
+			println "Error"
+		}
 	}
 
 	def progress25(){
@@ -38,6 +57,10 @@ class Level1Loop {
 		def b = Math.abs(new Random().nextInt() % 6) + 8
 
 		def answer = System.console().readLine a + ' + ' + b +' = '
+
+		if(!checksum(answer.toInteger(), a, b)){
+			println "Error"
+		}
 	}
 
 	def progress50(){
@@ -45,6 +68,10 @@ class Level1Loop {
 		def b = Math.abs(new Random().nextInt() % a) + 1
 
 		def answer = System.console().readLine a + ' - ' + b +' = '
+
+		if(!checkdiff(answer.toInteger(), a, b)){
+			println "Error"
+		}
 	}
 
 	def progress75(){
@@ -52,5 +79,9 @@ class Level1Loop {
 		def b = Math.abs(new Random().nextInt() % a) + 1
 
 		def answer = System.console().readLine a + ' - ' + b +' = '
+
+		if(!checkdiff(answer.toInteger(), a, b)){
+			println "Error"
+		}
 	}
 }
--- projects/m-math/m-math.groovy
+++ projects/m-math/m-math.groovy
@@ -9,8 +9,6 @@ def lessonSelect = System.console().readLine 'What level do you want 1-4? '
 def progression  = System.console().readLine 'Select progression 0, 0.25, 0.50, 0.75: '	
 //add a progression check here 		
 def lesson = new Lesson(lessonSelect.toInteger(), progression.toFloat())
-println lesson.getLevel()
-println lesson.getProgress()
 lesson.start()
 
 
